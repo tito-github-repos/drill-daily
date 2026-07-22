4daf0e595b9b6ff57eaa04301dbd13621b068f32
Manishasubburajs <manisha@tito.org.in>
2026-07-22 12:04:46 +0530

    next config security changes added

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
-import type { NextConfig } from "next";
-
 const csp = `
   default-src 'self';
   base-uri 'self';
